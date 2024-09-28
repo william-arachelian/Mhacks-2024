8130c3733146977682c83eaee29f281e7bf4ec00
Jared <jaredshi0@gmail.com>
2024-09-28 15:47:42 -0400

    set up button for recipeGenerator

---
+++ frontend/app/RecipeGenerator.jsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Button label="Recipe Generator" />
+      </View>
+    );
+  }
---
+++ frontend/app/index.jsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Button from '../components/button';
+import { Link } from 'expo-router';
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Link href="/RecipeGenerator">
+      <Button label="Recipe Generator"/>
+      </Link>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
