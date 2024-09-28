445b9c814d9c737583bbe48855c336f6e799f006
Will Arachelian <william.arachelian@gmail.com>
2024-09-28 14:23:01 -0400

    ingredients database functions

---
+++ backend/database/helpers.py
@@ -0,0 +1,34 @@
+from bson.objectid import ObjectId
+
+def is_valid_ingredient(input):
+
+        if input.get('name') == None:
+            print('name attribute does not exist')
+            return False
+        
+        if type(input['name']) is not str:
+            print('name attribute must be of type string')
+            return False
+        
+        if (input.get('quantity') != None and type(input['quantity']) is not int):
+            print('quantity attribute must be of type int')    
+            return False
+        
+        return True
+
+def is_valid_id(id):
+     
+
+    if type(id) is not str:
+        return False
+     
+    try:
+        obj_id = ObjectId(id)
+        return True
+    
+    except Exception as e:
+        print(e)
+        return False
+    
+        
+
