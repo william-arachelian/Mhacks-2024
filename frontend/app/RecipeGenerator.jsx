27d78957c8f1453c93f80dc865e1562b9a3719ff
Ethan Chen <cheneth@umich.edu>
2024-09-28 19:16:26 -0400

    frontpage and navigation bar

--- frontend/app/RecipeGenerator.jsx
+++ frontend/app/RecipeGenerator.jsx
@@ -1,9 +1,26 @@
-export default function Page() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-        <Button label="Recipe Generator" />
-      </View>
-    );
-  }
+import React from 'react';
+import { ImageBackground, View, Text, StyleSheet } from 'react-native';
+import background_image from '../assets/background_image.jpg'; // Adjust the path if necessary
+
+export default function RecipeGenerator() {
+  return (
+    <ImageBackground source={background_image} resizeMode="cover" style={styles.background_image}>
+        <View style={styles.container}>
+        <Text style={styles.text}>Recipe Generator</Text>
+        </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background_image: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
