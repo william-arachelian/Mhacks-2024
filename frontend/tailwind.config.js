13645c8c3cf1dd9be088ed54b6af3f3aac0970cf
Jared Shi <119568837+jaredshi0@users.noreply.github.com>
2024-09-29 02:52:49 -0400

    Revert "Frontend touchups"

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  content: [],
-    content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
