e23edd47778d0b378daad39c6454a980751f978f
Jared <jaredshi0@gmail.com>
2024-09-28 23:45:45 -0400

    make recipe page and loading screen

--- frontend/app/RecipeGenerator.jsx
+++ frontend/app/RecipeGenerator.jsx
@@ -1,30 +1,71 @@
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import TextInputExample from '../components/textbox';
+import { Image,ActivityIndicator, ImageBackground, StyleSheet, Text, View, } from 'react-native';
+import React, { useState } from 'react';
 import { ListItem} from 'react-native-elements';
 import { router } from 'expo-router';
 import background_image from '../assets/background_image.jpg'; // Adjust the path if necessary
+import * as SplashScreen from 'expo-splash-screen';
 
-const list = [
-  {
+
+SplashScreen.preventAutoHideAsync();
+
+export default function Page() {
+  const [list, setList] = useState(([{
     name: 'food',
   },
   {
     name: 'sandwich'
   },
   {
-    name: 'ga'
-  }
-] 
+    name: 'foods'
+  },]))
 
-export default function Page() {
+  const [appIsReady, setAppIsReady] = React.useState(false);
+  React.useEffect(() => {
+    setAppIsReady()
+    async function prepare() {
+      try {
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, [list]);
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return(
+      <View style= {styles.waiting}>
+        <Text>cooking your food</Text>
+        <Image source={require('../assets/foodloading.gif')} style={{ width: 200, height: 200 }} />
+        </View>
+    )
+  }
+
+
 
   return (
+    
     <ImageBackground source={background_image} resizeMode="cover" style={styles.background_image}>
 
-        <View>
-        <Text style= {styles.title}> Recipe Generation </Text>
-        <TextInputExample>textInput</TextInputExample>
+        <View onLayout={onLayoutRootView}>
+        <Text style= {styles.title}> Recipe List </Text>
         {
         list.map((item, i) => {
           return <ListItem button onPress={()=> router.navigate("../recipe")} key={i} bottomDivider>
@@ -56,8 +97,16 @@ export default function Page() {
     title: {
       color:"#000",
       textAlign: "center",
-      padding: 24
+      fontSize:24,
+      padding: 24,
+      backgroundColor:"#dddddd"
     },
 
+    waiting:
+    {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
   });
   
